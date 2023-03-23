764dcf74281795833eedf842b550431bc796d5d4
busra-yildiz <128553081+busra-yildiz@users.noreply.github.com>
2023-03-24 02:37:39 +0300

    Add files via upload

---
+++ yer işaretleri kitabı son uygulama/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  LandmarkBook
+//
+//  Created by A on 23.03.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var landmarkLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+    }
+    
+
+   
+    }
+
+
+
